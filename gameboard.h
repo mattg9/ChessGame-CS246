c097874445f8908afa86bc613c7a9e2baa6e34fd
Matthew <matthewgaston9@gmail.com>
2014-07-21 18:13:59 -0400

    Update gameboard.h

--- gameboard.h
+++ gameboard.h
@@ -10,12 +10,21 @@
 #include <string>
 #include "gamepiece.h"
 
+struct Move {
+	// initial pos
+	Pos pos1;
+	// final pos
+	Pos pos2;
+};
+
 class GameBoard {
 	public:
 	~GameBoard();
 	// singleton constructor
 	static GameBoard *startGame();	
+	// bool isLegalMove(Move m)
 	bool isLegalMove(char x1, int y1, char x2, int y2);
+	// void move(Move m)
 	void move(char x1, int y1, char x2, int y2);
 	friend std::ostream& operator<<(std::ostream &out, GameBoard &gb);
 	
@@ -25,5 +34,6 @@ class GameBoard {
 	static GameBoard *gbInstance;
 	static double whiteWins;
 	static double blackWins;
+	// static Move *listMoves;
 };
 #endif
