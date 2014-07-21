d5430ad969fd8957b994686d18b86a3834f90264
Matthew <matthewgaston9@gmail.com>
2014-07-21 19:23:16 -0400

    Update gameboard.cc

--- gameboard.cc
+++ gameboard.cc
@@ -24,48 +24,124 @@ GameBoard* GameBoard::gbInstance = NULL;
 
 // one gameboard exists at any time
 // this method creates gameboard
-GameBoard *GameBoard::startGame(){
+GameBoard *GameBoard::startGame(string init){
 	if (gbInstance == NULL){
 		gbInstance = new GameBoard();
-		return gbInstance;
+	} 
+	if (init == "default") {
+		defaultGameBoard();
+	} else if (init == "setup") {
+		clearBoard();
 	}
+	return gbInstance;
 }
 
 
-// constructing the gameboard	
-GameBoard::GameBoard(){
+// clear gameboard for steup mode
+void GameBoard::clearBoard(){
+	for (int i = 1; i < 9; i++) {
+		for (char j = 'a'; j <= 'h'; j++){
+			if (piece[i][j-97] != NULL) {
+				delete piece[i][j-97];
+			}
+			piece[i][j-97] = new EmptySpace(j,i);	
+		}
+	}
+}
+
+void GameBoard::add(char gamepiece, Pos p) {
+	delete piece[p.y][p.x];
+	string player;
+	if (gamepiece >= 'a' && gamepiece <= 'z') {
+		player = "black";
+	} else {
+		player = "white";
+	}
+	if (gamepiece == 'p' || gamepiece == 'P') {
+		piece[p.y][p.x] = new Pawn(p, player);
+	} else if (gamepiece == 'r' || gamepiece == 'R') {
+		piece[p.y][p.x] = new Rook(p,player);
+	} else if (gamepiece == 'q' || gamepiece == 'Q') {
+		piece[p.y][p.x] = new Queen(p,player);
+	} else if (gamepiece == 'k' || gamepiece == 'K') {
+		piece[p.y][p.x] = new King(p,player);
+	} else if (gamepiece == 'b' || gamepiece == 'B') {
+		piece[p.y][p.x] = new Bishop(p,player);
+	} else if (gamepiece == 'n' || gamepiece == 'N') {
+		piece[p.y][p.x] = new Knight(p,player);
+	}
+}
+
+void GameBoard::del(Pos p) {
+	delete piece[p.y][p.x];
+	piece[p.y][p.x] = new EmptySpace(p);
+}
+
+bool GameBoard::validBoard(){
+	countBlackKing = 0;
+	countWhiteKing = 0;
 	for (int i = 1; i < 9; i++) {
 		for (char j = 'a'; j <= 'h'; j++) {
+			if (piece[i][j-97]->getName() == "K") {
+				countWhiteKing++;
+			} else if (piece[i][j-97]->getName() == "k") {
+				countBlackKing++;
+			}
+			if (piece[i][j-97]->getName() == "p" || piece[i][j-97]->getName() == "P") {
+				if (i == 1 || i == 8) {
+					return false;
+				}
+			}
+		}
+	}
+	if (countWhiteKing != 1 || countBlackKing != 1) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+GameBoard::GameBoard(){}
+// constructing the default gameboard	
+void GameBoard::defaultGameBoard(){
+	for (int i = 1; i < 9; i++) {
+		for (char j = 'a'; j <= 'h'; j++) {
+			if (piece[i][j-97] != NULL) {
+				delete piece[i][j-97];
+			}
+			Pos p;
+			p.x = j;
+			p.y = i;
 			if (i == 1) {
 				if (j == 'a' || j == 'h') {
-					piece[i][j-97] = new Rook(j,i,"white");
+					piece[i][j-97] = new Rook(p,"white");
 				} else if (j == 'b' || j == 'g') {
-					piece[i][j-97] = new Knight(j,i,"white");
+					piece[i][j-97] = new Knight(p,"white");
 				} else if (j == 'c' || j == 'f') {
-					piece[i][j-97] = new Bishop(j,i,"white");
+					piece[i][j-97] = new Bishop(p,"white");
 				} else if (j == 'd') {
-					piece[i][j-97] = new Queen(j,i,"white");
+					piece[i][j-97] = new Queen(p,"white");
 				} else {
-					piece[i][j-97] = new King(j,i,"white");
+					piece[i][j-97] = new King(p,"white");
 				}
 			} else if (i == 2) {
-				piece[i][j-97] = new Pawn(j,i, "white");
+				piece[i][j-97] = new Pawn(p, "white");
 			} else if (i == 7) {
-				piece[i][j-97] = new Pawn(j,i,"black");
+				piece[i][j-97] = new Pawn(p,"black");
 			} else if (i == 8) {
 				if (j == 'a' || j == 'h') {
-					piece[i][j-97] = new Rook(j,i,"black");
+					piece[i][j-97] = new Rook(p,"black");
 				} else if (j == 'b' || j == 'g') {
-					piece[i][j-97] = new Knight(j,i,"black");
+					piece[i][j-97] = new Knight(p,"black");
 				} else if (j == 'c' || j == 'f') {
-					piece[i][j-97] = new Bishop(j,i,"black");
+					piece[i][j-97] = new Bishop(p,"black");
 				} else if (j == 'd') {
-					piece[i][j-97] = new Queen(j,i,"black");
+					piece[i][j-97] = new Queen(p,"black");
 				} else {
-					piece[i][j-97] = new King(j,i,"black");
+					piece[i][j-97] = new King(p,"black");
 				}
 			} else { 
-				piece[i][j-97] = new EmptySpace(j,i);
+				piece[i][j-97] = new EmptySpace(jp);
 			}
 		}
 	}
@@ -80,7 +156,7 @@ GameBoard::~GameBoard(){
 	}
 }
 
-// pint the gameboard
+// output the gameboard
 ostream& operator<<(ostream &out, GameBoard &gb){
 	for (int i = 8; i > 0; i--) {
 		out << i;
@@ -93,27 +169,28 @@ ostream& operator<<(ostream &out, GameBoard &gb){
 	return out;
 }
 
-bool GameBoard::isLegalMove(char x1, int y1, char x2, int y2){
+bool GameBoard::isLegalMove(Move &m){
  	// check for pieces in the way!
 	// does the piece move like this!
 	//piece[y1][x2-97]->isLegalMove(x2,y2);
 	return true;
 }
 
-void GameBoard::move(char x1, int y1, char x2, int y2) {
-	string p1 = piece[y1][x1-97]->getUnoccupied();
-	string p2 = piece[y2][x2-97]->getUnoccupied();
-	piece[y1][x1-97]->setUnoccupied(p2);
-	piece[y2][x2-97]->setUnoccupied(p1);
+// move game pieces
+void GameBoard::move(Move &m) {
+	string p1 = piece[m.pos1.y][m.pos1.x-97]->getUnoccupied();
+	string p2 = piece[m.pos2.y][m.pos2.x-97]->getUnoccupied();
+	piece[m.pos1.y][m.pos1.x-97]->setUnoccupied(p2);
+	piece[m.pos2.y][m.pos2.x-97]->setUnoccupied(p1);
 	GamePiece *temp;
 	// difference between a move and undo
 	// undo reverts to last state
 	// move will change captured to true
-	if (piece[y2][x2-97]->getName() != "empty"
-		&& !piece[y2][x2-97]->getCaptured()) {
-		piece[y2][x2-97]->Captured();
+	if (piece[m.pos2.y][m.pos2.x-97]->getName() != "empty"
+		&& !piece[m.pos2.y][m.pos2.x-97]->getCaptured()) {
+		piece[m.pos2.y][m.pos2.x-97]->Captured();
 	}
-	temp = piece[y1][x1-97];
-	piece[y1][x1-97] = piece[y2][x2-97];
-	piece[y2][x2-97] = temp;
+	temp = piece[m.pos1.y][m.pos1.x-97];
+	piece[m.pos1.y][m.pos1.x-97] = piece[m.pos2.y][m.pos2.x-97];
+	piece[m.pos2.y][m.pos2.x-97] = temp;
 }
