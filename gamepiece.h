139b1f3884931935ed980b565d0c3ec05b193b87
Matthew <matthewgaston9@gmail.com>
2014-07-21 19:36:17 -0400

    Update gamepiece.h

--- gamepiece.h
+++ gamepiece.h
@@ -18,16 +18,16 @@ struct Pos {
 class GamePiece {
 	public:
 	// constructor and destructor
-	GamePiece(char x, int y);
+	GamePiece(Pos &p);
 	virtual ~GamePiece();
 	// is this move legal with how this piece operates?
-	//virtual bool isLegalMove(Position pos1, Position pos2) = 0; 
+	//virtual bool isLegalMove(Move &m) = 0; 
 	virtual void Captured() = 0;
 	virtual void print(std::ostream& out) const = 0;
 	// assign string value for the gameboard box colour below piece
 	// mutator functions for initializing, and in game changing
 	// of pieces
-	void unoccupiedValue(char x, int y);
+	void unoccupiedValue(Pos &p);
 	void setUnoccupied(std::string str);
 	// accessor functions
 	std::string getUnoccupied();
@@ -37,8 +37,7 @@ class GamePiece {
 	std::string name; // gamepiece name
 	std::string unoccupied; // either a white box " " or black "_" position
 	// position of gamepiece
-	char x;
-	int y;
+	Pos p;
 	
 };
 #endif
