4ffcf32237cbee9148efd424c3d677c94165a3e3
Matthew <matthewgaston9@gmail.com>
2014-07-21 20:32:04 -0400

    Update gameboard.h

--- gameboard.h
+++ gameboard.h
@@ -24,7 +24,7 @@ class GameBoard {
 	static GameBoard *startGame(std::string init);
 	void clearBoard();
 	void printScore();
-	bool isLegalMove(Move &m)
+	bool isLegalMove(Move &m, bool inCheck, std::string colour)
 	void move(Move &m)
 	friend std::ostream& operator<<(std::ostream &out, GameBoard &gb);
 	
